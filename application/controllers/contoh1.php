24a37ee465846c011be3a603f2147fcde5078724
Taufikhfd <taufikhidayatafandi@gmail.com>
2024-03-28 20:13:16 +0700

    New Repository dan contoh1,contoh2,contoh3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Taufk Hidayat Kelas 15.4B.05
+saya asli dari tasikmalaya tinggal di cikarang";
+}
+}
+?>
