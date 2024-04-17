6e93b7b405329f354ad29dbe4811279036ab720c
Taufikhfd <taufikhidayatafandi@gmail.com>
2024-04-17 08:34:28 +0700

    Menambahkan Tuas Rentalbuku.Net

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?= $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Taufik</li>
+        <li>Nama Belakang : Hidayat</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Tasikmalaya Rancapaku Gununggalunggung</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tasikmalaya</li>
+    </ul>
+
+    <h4>Olah raga Favorit </h4>
+    <ul type="square">
+        <li>Mendaki</li>
+        <li>Riding</li>
+    </ul>
+</section>
